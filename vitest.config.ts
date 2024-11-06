67c41142c9cf6e2ee51e6f83f4085a22d3449a45
Jefferson Silva <jeffsilva01.dev@gmail.com>
2024-11-06 17:22:07 -0300

    It improves the test configuration by allowing the use of the '@' alias in test files.
    - Add path alias '@' pointing to './src' directory
    - Import 'path' module from Node.js
    - Enhance test configuration for better import resolution

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from "vitest/config";
+import path from "node:path";
 
 export default defineConfig({
   test: {
     globals: true,
     setupFiles: ["./src/shared/infra/testing/expect-helpers.ts"],
     include: ["**/*.{test,int-spec,spec}.?(c|m)[jt]s?(x)"],
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
 });
