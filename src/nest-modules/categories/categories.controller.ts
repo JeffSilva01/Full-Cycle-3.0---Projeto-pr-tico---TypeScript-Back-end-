064a1aeb0c031cae021cd13822f4e94b75dcd000
Jefferson Silva <jeffsilva01.dev@gmail.com>
2024-11-30 13:52:18 -0300

    feat(categories): Implement update category endpoint
    - Add ParseUUIDPipe to validate and parse the category ID
    - Implement update method in CategoriesController
    - Update UpdateCategoryDto to extend UpdateCategoryInputWithoutId
    - Use OmitType to create UpdateCategoryInputWithoutId without 'id' field

--- src/nest-modules/categories/categories.controller.ts
+++ src/nest-modules/categories/categories.controller.ts
@@ -7,6 +7,7 @@ import {
   Param,
   Delete,
   Inject,
+  ParseUUIDPipe,
 } from '@nestjs/common';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
@@ -49,10 +50,17 @@ export class CategoriesController {
   findOne(@Param('id') id: string) {}
 
   @Patch(':id')
-  update(
-    @Param('id') id: string,
+  async update(
+    @Param('id', new ParseUUIDPipe({ errorHttpStatusCode: 422 })) id: string,
     @Body() updateCategoryDto: UpdateCategoryDto,
-  ) {}
+  ) {
+    const result = await this.updateUseCase.execute({
+      ...updateCategoryDto,
+      id,
+    });
+
+    return CategoriesController.serialize(result);
+  }
 
   @Delete(':id')
   remove(@Param('id') id: string) {}
