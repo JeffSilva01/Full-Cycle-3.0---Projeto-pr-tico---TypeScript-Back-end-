9593978d7c2f943b7d81f183ba7704a86a1578a3
Jefferson Silva <jeffsilva01.dev@gmail.com>
2024-11-30 13:15:45 -0300

    refactor: Implement Clean Architecture in Categories module
    - Update CategoriesController to use Use Cases instead of direct
    repository access- Add Use Case injections in CategoriesController
    - Implement Create Category endpoint using CreateCategoryUseCase
    - Update CategoriesModule to include providers for repositories and use
    cases
    - Extend CreateCategoryDto from CreateCategoryInput
    
    - Add global ValidationPipe in main.ts- Format code and update import
    styles

--- src/nest-modules/categories/categories.controller.ts
+++ src/nest-modules/categories/categories.controller.ts
@@ -6,19 +6,39 @@ import {
   Patch,
   Param,
   Delete,
+  Inject,
 } from '@nestjs/common';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
-import { CategorySequelizeRepository } from '@/core/category/infra/db/sequelize/category-sequelize.repository';
+import { CreateCategoryUseCase } from '@/core/category/application/use-case/create-category/create-category.use-case';
+import { UpdateCategoryUseCase } from '@/core/category/application/use-case/update-category/update-category.use-case';
+import { GetCategoryUseCase } from '@/core/category/application/use-case/get-category/get-category.use-case';
+import { DeleteCategoryUseCase } from '@/core/category/application/use-case/delete-category/delete-category.use-case';
+import { ListCategoriesUseCase } from '@/core/category/application/use-case/list-category/list-category.use-case';
 
 @Controller('categories')
 export class CategoriesController {
-  constructor(private categoryRepo: CategorySequelizeRepository) {
-    console.log(categoryRepo);
-  }
+  @Inject(CreateCategoryUseCase)
+  private createUseCase: CreateCategoryUseCase;
+
+  @Inject(UpdateCategoryUseCase)
+  private updateUseCase: UpdateCategoryUseCase;
+
+  @Inject(GetCategoryUseCase)
+  private getUseCase: GetCategoryUseCase;
+
+  @Inject(DeleteCategoryUseCase)
+  private deleteUseCase: DeleteCategoryUseCase;
+
+  @Inject(ListCategoriesUseCase)
+  private listeUseCase: ListCategoriesUseCase;
 
   @Post()
-  create(@Body() createCategoryDto: CreateCategoryDto) {}
+  create(@Body() createCategoryDto: CreateCategoryDto) {
+    const result = this.createUseCase.execute(createCategoryDto);
+
+    return result;
+  }
 
   @Get()
   findAll() {}
--- src/nest-modules/categories/categories.module.ts
+++ src/nest-modules/categories/categories.module.ts
@@ -1,19 +1,20 @@
-import { Module } from '@nestjs/common';
-import { CategoriesController } from './categories.controller';
-import { getModelToken, SequelizeModule } from '@nestjs/sequelize';
-import { CategoryModel } from '@/core/category/infra/db/sequelize/category.model';
-import { CategorySequelizeRepository } from '@/core/category/infra/db/sequelize/category-sequelize.repository';
+import { Module } from "@nestjs/common";
+import { CategoriesController } from "./categories.controller";
+import { SequelizeModule } from "@nestjs/sequelize";
+import { CategoryModel } from "@/core/category/infra/db/sequelize/category.model";
+import { CATEGORY_PROVIDERS } from "./categories.providers";
 
 @Module({
-  imports: [SequelizeModule.forFeature([CategoryModel])],
-  controllers: [CategoriesController],
-  providers: [
-    {
-      provide: CategorySequelizeRepository,
-      useFactory: (categoryModel: typeof CategoryModel) =>
-        new CategorySequelizeRepository(categoryModel),
-      inject: [getModelToken(CategoryModel)],
-    },
-  ],
+	imports: [SequelizeModule.forFeature([CategoryModel])],
+	controllers: [CategoriesController],
+	providers: [
+		...Object.values(CATEGORY_PROVIDERS.REPOSITORIES),
+		...Object.values(CATEGORY_PROVIDERS.USE_CASES),
+		// ...Object.values(CATEGORY_PROVIDERS.VALIDATIONS),
+	],
+	exports: [
+		CATEGORY_PROVIDERS.REPOSITORIES.CATEGORY_REPOSITORY.provide,
+		// CATEGORY_PROVIDERS.VALIDATIONS.CATEGORIES_IDS_EXISTS_IN_DATABASE_VALIDATOR.provide,
+	],
 })
 export class CategoriesModule {}
